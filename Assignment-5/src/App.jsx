59d55ee962cf0d159413d63297ec2389816af4cb
Charan-teja16 <22r11a05x7@gcet.edu.in>
2026-01-10 20:03:11 +0530

    Add files via upload

--- Assignment-5/src/App.jsx
+++ Assignment-5/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
+import { ProductProvider } from './context/ProductContext'
 import Page1 from './pages/Page1'
 import Page2 from './pages/Page2'
 import 'antd/dist/reset.css'
@@ -8,12 +9,14 @@ import 'antd/dist/reset.css'
 function App() {
   return (
     <ConfigProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-        </Routes>
-      </BrowserRouter>
+      <ProductProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Page1 />} />
+            <Route path="/page2" element={<Page2 />} />
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
     </ConfigProvider>
   )
 }
